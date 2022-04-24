30710f4f91c5caa8b15779ab29728be5f043f08b
Cristopher Bohol <cristopher.bohol.21@usjr.edu.ph>
2022-04-24 19:16:22 +0800

    assignment??

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+describe('AppComponent - Bohol, Cristopher B.', () => {
   let fixture: ComponentFixture<AppComponent>;
   let component: AppComponent;
 
@@ -19,42 +19,94 @@ describe('AppComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should click `Use Letters` and useLetters should be true', () => {
-    expect(component.useLetters).toBeFalse();
+  it('should return the parsed length', () => {
+    expect(component.pwLength).toBe(0);
 
     let input = fixture.debugElement.query(
-      By.css('#withLetters')
+      By.css('#pwLenght')
     ).nativeElement;
 
-    input.click();
+    input.value = 10;
+    input.dispatchEvent(new Event('input'));
     fixture.detectChanges();
 
-    expect(component.useLetters).toBeTrue();
+    expect(component.pwLength).toBe(10);
   });
 
-  it('should click `Use Symbols` and useSymbols should be true', () => {
-    expect(component.useSymbols).toBeFalse();
+  describe('when clicking the checkbox', () => {
+    it('should call `onChangeUseLetters`', () => {
+      spyOn(component, 'onChangeUseLetters');
+    });
 
-    let input = fixture.debugElement.query(
-      By.css('#withSymbols')
-    ).nativeElement;
+    it('should call `onChangeUseNumbers`', () => {
+      spyOn(component, 'onChangeUseNumbers');
+    });
 
-    input.click();
-    fixture.detectChanges();
-
-    expect(component.useSymbols).toBeTrue();
+    
+    it('should call `onChangeUseSymbols`', () => {
+      spyOn(component, 'onChangeUseSymbols');
+    });
+  
+    it('should click `Use Letters` and useLetters should be true', () => {
+      expect(component.useLetters).toBeFalse();
+  
+      let input = fixture.debugElement.query(
+        By.css('#withLetters')
+      ).nativeElement;
+  
+      input.click();
+      fixture.detectChanges();
+      expect(component.useLetters).toBeTrue();
+    });
+  
+    it('should click `Use Symbols` and useSymbols should be true', () => {
+      expect(component.useSymbols).toBeFalse();
+  
+      let input = fixture.debugElement.query(
+        By.css('#withSymbols')
+      ).nativeElement;
+  
+      input.click();
+      fixture.detectChanges();
+  
+      expect(component.useSymbols).toBeTrue();
+    });
+  
+    it('should click `Use Numbers` and useNumbers should be true', () => {
+      expect(component.useNumbers).toBeFalse();
+  
+      let input = fixture.debugElement.query(
+        By.css('#withNumbers')
+      ).nativeElement;
+  
+      input.click();
+      fixture.detectChanges();
+  
+      expect(component.useNumbers).toBeTrue();
+    });
   });
 
-  it('should click `Use Numbers` and useNumbers should be true', () => {
-    expect(component.useNumbers).toBeFalse();
+  describe('when clicking the button', () => {
+    it('should call `onButtonClick`', () => {
+      spyOn(component, 'onButtonClick');
+    });
 
-    let input = fixture.debugElement.query(
-      By.css('#withNumbers')
-    ).nativeElement;
-
-    input.click();
-    fixture.detectChanges();
-
-    expect(component.useNumbers).toBeTrue();
+    it('should return the generated password', () => {
+      expect(component.password).toBe('');
+      
+      component.onButtonClick();
+  
+      expect(component.password).not.toBeNull;
+    }); 
   });
+
+  // describe('display the result', () => {
+  //   it('should display the result', () => {
+  //     const compiled = fixture.debugElement.nativeElement;
+  //     expect(compiled.querySelector('h1').textContent).toContain(
+  //       ''
+  //     );
+  //   });
+  // });
+
 });
